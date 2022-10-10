655051250f675e429fb36a18c0f026c110eaf00f
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-10-10 03:26:12 -0500

    Fix packaging for 'google-chrome-stable'

--- scripts/google-chrome-stable.sh
+++ scripts/google-chrome-stable.sh
@@ -2,7 +2,7 @@
 set -e
 
 wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -O chrome.deb
-sudo apt install ./chrome.deb
+sudo apt install ./chrome.deb -y
 sudo apt update
 
 mpr_version="$(util/get-pkgbuild-version.sh)"
