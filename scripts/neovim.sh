13cd3307a70ddffb55faa5f980f3a9f82bba4898
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-10-29 22:32:23 -0500

    Fix updating script for Neovim

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
+# We can't use 'util/get-latest-gh-version.sh' due to how Neovim names their releases.
 mpr_version="$(util/get-pkgbuild-version.sh)"
-gh_version="$(util/get-latest-gh-version.sh 'neovim/neovim')"
+gh_version="$(gh release list -R neovim/neovim --exclude-drafts | sed -E 's/^Nvim |^NVIM //' | grep '^v' | awk '{print $1}' | head -n 1 | sed 's|^v||')"
 
 if [[ "${gh_version}" != "${mpr_version}" ]]; then
     util/update-pkgbuild-version.sh "${gh_version}"
