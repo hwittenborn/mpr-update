dab0adf3ffe2294dbe0e3b27267d1c9a417cd8f7
Hunter Wittenborn <hunter@hunterwittenborn.com>
2023-02-26 14:53:15 -0600

    Fix version checks for 'neovim'

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -3,7 +3,7 @@ set -eu
 
 # We can't use 'util/get-latest-gh-version.sh' due to how Neovim names their releases.
 mpr_version="$(util/get-pkgbuild-version.sh)"
-gh_version="$(gh release list -R neovim/neovim --exclude-drafts | sed -E 's/^Nvim |^NVIM //' | grep '^v' | awk '{print $1}' | head -n 1 | sed 's|^v||')"
+gh_version="$(gh release list -R neovim/neovim --exclude-drafts | sed -E 's/^Nvim |^NVIM //' | grep '^[v0-9]' | awk '{print $1}' | head -n 1 | sed 's|^v||')"
 
 if [[ "${gh_version}" != "${mpr_version}" ]]; then
     util/update-pkgbuild-version.sh "${gh_version}"
