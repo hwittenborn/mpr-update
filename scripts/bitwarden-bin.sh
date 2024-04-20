10015b762855d85c8c19412201ebb3cfef8a976f
Hunter Wittenborn <hunter@hunterwittenborn.com>
2024-04-19 21:47:42 -0500

    Add update functionality for 'glow'
    Also removed 'bitwarden-bin' since we don't maintain that any more.

--- scripts/bitwarden-bin.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-mpr_version="$(util/get-pkgbuild-version.sh)"
-repology_version="$(util/get-latest-repology-version.sh 'bitwarden')"
-
-if [[ "${repology_version}" != "${mpr_version}" ]]; then
-    util/update-pkgbuild-version.sh "${repology_version}"
-    util/push-changes.sh
-fi
